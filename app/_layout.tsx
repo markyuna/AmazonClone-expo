884a5bc009c3c1772b92134c00b371b96385d34f
MARCOS <marcossuarezr88@gmail.com>
2023-11-28 23:51:33 +0100

    apmlify configure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,8 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack, useLocalSearchParams, useRouter } from 'expo-router';
 import HeaderComponent from './components/HeaderComponent';
-// import HomeScreen from './(tabs)';
+
+
 
 export { ErrorBoundary } from 'expo-router';
 
